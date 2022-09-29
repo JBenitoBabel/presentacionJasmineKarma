115d588e30b600b7adc5b86328e6d5a7d41fefeb
Julia Benito <julia.benito@babelgroup.com>
2022-09-30 01:17:50 +0200

    spy test errors

--- src/app/medium-tests/spy/spy.page.spec.ts
+++ src/app/medium-tests/spy/spy.page.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { from, Observable } from 'rxjs';
+import { from, throwError } from 'rxjs';
 import { GlobalService } from 'src/app/services/global.service';
 
 import { SpyPage } from './spy.page';
@@ -41,7 +41,7 @@ describe('SpyPage', () => {
   it('Init get cats', () => {
     const cats = ['zan', 'norte'];
     spyOn(service, 'get').and.callFake(() => {
-      return from([cats])
+      return from([cats]);
     })
     component.ngOnInit();
     expect(component.cats.length).toBeGreaterThan(0);
@@ -49,7 +49,7 @@ describe('SpyPage', () => {
 
   it('addCats call service', () => {
     const spyGetCats = spyOn(service, 'post').and.callFake(cat => {
-      return from([null])
+      return from([null]);
     })
     component.addCats();
     expect(spyGetCats).toHaveBeenCalled();
@@ -57,10 +57,37 @@ describe('SpyPage', () => {
 
   it('addCats returns a value and adds it to array', () => {
     const cat = {id: 1, name: 'Zan'}
-    spyOn(service, 'post').and.returnValue(from([cat]))
+    spyOn(service, 'post').and.returnValue(from([cat]));
 
     component.addCats();
     expect(component.cats.indexOf(cat)).toBeGreaterThanOrEqual(0);
+  });
+
+  it('if addCats fails the error message should be the same as the server', () => {
+    const error = 'No se pudo añadir el gato';
+    spyOn(service, 'post').and.returnValue(throwError(error));
+    component.addCats();
+    expect(component.messageError).toBe(error);
+  });
+
+  xit('deleteCat must call the server to remove a cat', () => {
+    const spy = spyOn(service, 'delete').and.returnValue(from([null]));
+    component.deleteCat('1');
+    expect(spy).toHaveBeenCalledWith('1', 'cats'); // obliga a intervenir al usuario
+  });
+
+  it('deleteCat must call the server to remove a cat', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    const spy = spyOn(service, 'delete').and.returnValue(from([null]));
+    component.deleteCat('1');
+    expect(spy).toHaveBeenCalledWith('1', 'cats');
+  });
+
+  it('deleteCat NOT must call the server to remove a cat', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    const spy = spyOn(service, 'delete').and.returnValue(from([null]));
+    component.deleteCat('1');
+    expect(spy).not.toHaveBeenCalledWith('1', 'cats');
   });
 
 });
